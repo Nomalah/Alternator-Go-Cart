b98ad2940d47645a334ed93a040f3010e6646651
joonsauce <77248354+joonsauce@users.noreply.github.com>
2022-07-07 21:55:47 -0400

    pin setups

--- E-MCU/E-MCU.ino
+++ E-MCU/E-MCU.ino
@@ -1,3 +1,8 @@
+#include "pins.h"
+
 void setup() {
-    
+    pinMode(RPMPin, INPUT);
+    pinMode(currentOut1, OUTPUT);
+    pinMode(currentOut2, OUTPUT);
+    pinMode(currentOut3, OUTPUT);
 }
---
+++ E-MCU/pins.h
@@ -0,0 +1,8 @@
+const int eGearPin = 1;
+const int RPMPin = 2;
+const int voltPin = 3;
+const int currentIn1 = 4;
+const int currentIn2 = 5;
+const int currentOut1 = 6;
+const int currentOut2 = 7;
+const int currentOut3 = 8;
