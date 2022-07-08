e42961bdd5b2213a45a9f6ec200003f1959baf4d
joonsauce <77248354+joonsauce@users.noreply.github.com>
2022-07-07 22:52:03 -0400

    more pin setup

--- E-MCU/E-MCU.ino
+++ E-MCU/E-MCU.ino
@@ -1,8 +1,28 @@
 #include "pins.h"
+#include "LiquidCrystal.h"
+int eGearStat;
+int voltVal;
+int current1;
+int current2;
+int M_MCU;
+// int throttleStat;
+// int eBrakeStat;
+// int rearBrakeStat;
 
 void setup() {
     pinMode(RPMPin, INPUT);
-    pinMode(currentOut1, OUTPUT);
-    pinMode(currentOut2, OUTPUT);
-    pinMode(currentOut3, OUTPUT);
+    pinMode(currentOutPin1, OUTPUT);
+    pinMode(currentOutPin2, OUTPUT);
+    pinMode(currentOutPin3, OUTPUT);
+}
+
+void loop() {
+    eGearStat = analogRead(eGearPin);
+    voltVal = analogRead(voltPin);
+    current1 = analogRead(currentInPin1);
+    current2 = analogRead(currentInPin2);
+    M_MCU = analogRead(serIn);
+    // throttleStat = analogRead(throttlePin);
+    // eBrakeStat = analogRead(eBrakePin);
+    // rearBrakeStat = analogRead(rearBrakePin);
 }
--- E-MCU/pins.h
+++ E-MCU/pins.h
@@ -1,8 +1,14 @@
 const int eGearPin = 1;
 const int RPMPin = 2;
 const int voltPin = 3;
-const int currentIn1 = 4;
-const int currentIn2 = 5;
-const int currentOut1 = 6;
-const int currentOut2 = 7;
-const int currentOut3 = 8;
+const int currentInPin1 = 4;
+const int currentInPin2 = 5;
+const int currentOutPin1 = 6;
+const int currentOutPin2 = 7;
+const int currentOutPin3 = 8;
+const int serIn = 9;
+const int serOut = 10;
+
+// const int throttlePin = 11;
+// const int eBrakePin = 12;
+// const int rearBrakePin = 13;
