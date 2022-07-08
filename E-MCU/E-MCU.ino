aba18e42120dae09085e136e3285a6cf974bd944
joonsauce <77248354+joonsauce@users.noreply.github.com>
2022-07-07 23:27:37 -0400

    LCD setup

--- E-MCU/E-MCU.ino
+++ E-MCU/E-MCU.ino
@@ -1,5 +1,6 @@
 #include "pins.h"
-#include "LiquidCrystal.h"
+#include <LiquidCrystal.h>
+
 int eGearStat;
 int voltVal;
 int current1;
@@ -9,11 +10,14 @@ int M_MCU;
 // int eBrakeStat;
 // int rearBrakeStat;
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup() {
     pinMode(RPMPin, INPUT);
     pinMode(currentOutPin1, OUTPUT);
     pinMode(currentOutPin2, OUTPUT);
     pinMode(currentOutPin3, OUTPUT);
+    lcd.begin(16, 2);
 }
 
 void loop() {
