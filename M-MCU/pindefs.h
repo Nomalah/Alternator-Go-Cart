b4434ecbfeaa4235552f875c3dfee033765ad0cc
Nomalah <36043275+Nomalah@users.noreply.github.com>
2022-07-09 13:30:17 -0400

    First Motor controler code

---
+++ M-MCU/pindefs.h
@@ -0,0 +1,51 @@
+#pragma once
+
+// Serial pins
+#define E_MCU_RX 0
+#define E_MCU_TX 0
+
+// (H)all (S)ensor pins
+#define HS_A 0
+#define HS_B 0
+#define HS_C 0
+#define HS_D 0
+#define HS_E 0
+#define HS_F 0
+
+// (H)igh side and (L)ow side driver (I)nput pins
+#define HI_A 0
+#define LI_A 0
+
+#define HI_B 0
+#define LI_B 0
+
+#define HI_C 0
+#define LI_C 0
+
+#define HI_D 0
+#define LI_D 0
+
+#define HI_E 0
+#define LI_E 0
+
+#define HI_F 0
+#define LI_F 0
+
+// (BEMF) pins
+#define HI_A 0
+#define LI_A 0
+
+#define HI_B 0
+#define LI_B 0
+
+#define HI_C 0
+#define LI_C 0
+
+#define HI_D 0
+#define LI_D 0
+
+#define HI_E 0
+#define LI_E 0
+
+#define HI_F 0
+#define LI_F 0
---
+++ message.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#pragma pack(push, 1)
+struct message { // 3-bytes
+    bool reverse; // 1-byte
+    bool ebrake; // 1-byte
+    char throttle; // 1-byte
+};
+#pragma pack(pop)
+
+static_assert(sizeof(message) == 3);
