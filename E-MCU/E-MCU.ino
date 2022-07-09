8206065fa125a5d1d5ad7a01ae95d95bf36fede5
joonsauce <77248354+joonsauce@users.noreply.github.com>
2022-07-09 13:46:19 -0400

    fixed pins & display; vscode arduino setup added

--- E-MCU/E-MCU.ino
+++ E-MCU/E-MCU.ino
@@ -1,14 +1,18 @@
 #include "pins.h"
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(38, 36, 35, 27, 25, 21);
+LiquidCrystal lcd(19, 23, 18, 17, 16, 15);
+
+bool reverseStat = false;
 
 void setup() {
     pinMode(RPMPin, INPUT);
+    pinMode(reversePin, INPUT);
     pinMode(currentOutPin1, OUTPUT);
     pinMode(currentOutPin2, OUTPUT);
     pinMode(currentOutPin3, OUTPUT);
     lcd.begin(16, 2);
+    Serial.begin(9600);
 }
 
 void loop() {
@@ -18,23 +22,35 @@ void loop() {
     int current1 = analogRead(currentInPin1);
     int current2 = analogRead(currentInPin2);
     int M_MCU = analogRead(serIn);
-    // int throttleStat = analogRead(throttlePin);
-    // int eBrakeStat = analogRead(eBrakePin);
-    // int rearBrakeStat = analogRead(rearBrakePin);
+    int throttleStat = analogRead(throttlePin);
+    int eBrakeStat = analogRead(eBrakePin);
+    int rearBrakeStat = analogRead(rearBrakePin);
+    lcd.print("RPM: ");
+    lcd.setCursor(5, 0);
+    int RPM = RPMSense(digitalRead(RPMPin));
+    lcd.print(RPM);
+    lcd.setCursor(0, 1);
+    lcd.print("TT: ");
+    lcd.setCursor(0, 0);
+    lcd.print(throttleStat);
 }
 
 void rotor() {
 
 }
 
-void RPMSense() {
+int RPMSense(int value) {
 
 }
 
-void eGearSend() {
+void eGear() {
 
 }
 
 void current() {
+
+}
+
+void reverse() {
 
 }
