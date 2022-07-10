0916accd68be233f79af68468ecd1cf4fac5a0fc
joonsauce <77248354+joonsauce@users.noreply.github.com>
2022-07-09 20:40:53 -0400

    fixed pin conflicts

--- E-MCU/pins.h
+++ E-MCU/pins.h
@@ -1,16 +1,28 @@
+#pragma once
+
 const int eGearPin = 36;
-const int RPMPin = 22;
-const int voltPin = 39;
-const int reversePin = 13;
+const int RPMPin = 32;
+const int voltPin = 18;
+
+// Motor related pins
+const int reversePin = 35;
+const int throttlePin = 33;
+const int eBrakePin = 26;
+const int rearBrakePin = 27;
+
+// LCD Pins
+#define LCD_RS 19
+#define LCD_EN 23
+#define LCD_D4 22   
+#define LCD_D5 21
+#define LCD_D6 17
+#define LCD_D7 16
+
+// placeholder pins
 const int currentInPin1 = 34;
 const int currentInPin2 = 35;
 const int currentOutPin1 = 2;
 const int currentOutPin2 = 4;
 const int currentOutPin3 = 5;
-const int serIn = 3;
-const int serOut = 1;
 const int rotorPin1 = 25;
 const int rotorPin2 = 26;
-const int throttlePin = 27;
-const int eBrakePin = 14;
-const int rearBrakePin = 12;
